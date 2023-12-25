8593efc8013604f6046f0217a90445089a3269fe
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2023-12-25 21:59:02 +0300

    rename

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { PlayerItem } from "./PlayerItem";
 
-export const PlayerList = ({ currentSymbol }) => {
+export const PlayerList = ({ symbol }) => {
   return (
     <ol id="players" className="highlight-player">
       <PlayerItem
         initialName="Player 1"
         symbol="X"
-        isActive={currentSymbol === "X" ? "active" : ""}
+        isActive={symbol === "X" ? "active" : ""}
       />
       <PlayerItem
         initialName="Player 2"
         symbol="O"
-        isActive={currentSymbol === "O" ? "active" : ""}
+        isActive={symbol === "O" ? "active" : ""}
       />
     </ol>
   );
